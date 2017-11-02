69cda8e7fa3f3eddf1fc4f714bf79d46c678b9ee
Dorian Abad Tovar Díaz <abadtovar@gmail.com>
2017-11-02 14:44:22 -0500

    Fixing errors in HTTP request for the device

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -19,7 +21,8 @@ import { AccountSettings } from '../pages/account-settings/account-settings';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(Lum)
+    IonicModule.forRoot(Lum),
+    HttpModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http, Headers } from '@angular/http';
 
 import { CreateProfile } from '../create-profile/create-profile'
+
 
 @Component({
   selector: 'page-home',
@@ -9,7 +11,7 @@ import { CreateProfile } from '../create-profile/create-profile'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http ) {
 
   }
 
@@ -17,4 +19,19 @@ export class HomePage {
     this.navCtrl.push( CreateProfile );
   }
 
+  sendDataToModule(){
+      var body = "intensidadDireccional=1&intensidadFrenado=2&frenadoContinuo=false";//{ intensidadDireccional: 1, intensidadFrenado: 1, frenadoContinuo: true };
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+      this.http.post( 'http://192.168.111.1/index.lua', body, { headers: headers } )
+      .subscribe(
+         data => {
+          console.log(data['_body']);
+        },
+         error => {
+          console.log(error);
+         }
+       );
+  }
 }
